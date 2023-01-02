828fd7023acf104f6a5d10a5f0ec4e0a27a01c0b
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-01-02 17:55:17 +0100

    Add some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
+//! Conversion between the Esperanto x-system and Unicode
+//! circumflexes.
+//!
+//! Provides a couple of utilites to convert between the Esperanto
+//! x-system (like “ehxosxangxocxiujxauxde”) and Unicode characters
+//! with circumflexes (like “eĥoŝanĝoĉiuĵaŭde”). When converting from
+//! the x-system, the X can be either a capital X or a lower-case x
+//! regardless of the case of the previous character. When converting
+//! to the X system the X will match the case of the Esperanto letter.
+//!
+//! The simplest way to use the Crate is with the functions
+//! [x_to_unicode] and [unicode_to_x]. These convert a string slice
+//! with one coding system to a String with the other. For example:
+//!
+//! ```
+//! use xsystem::{unicode_to_x, x_to_unicode};
+//!
+//! let unichars = x_to_unicode("acxajxo SxANGXEMA");
+//! assert_eq!(unichars, "aĉaĵo ŜANĜEMA".to_string());
+//!
+//! let xchars = unicode_to_x("terpomkaĉo estas AĈA");
+//! assert_eq!(xchars, "terpomkacxo estas ACXA".to_string());
+//! ```
+//!
+//! You can also use the functions [x_chars] and [unicode_chars] which
+//! adapt a char iterator to perform the conversion on the fly. For example:
+//!
+//! ```
+//! use xsystem::{x_chars, unicode_chars};
+//!
+//! let shouty_x = x_chars("li estas ĉarma".chars())
+//!     .map(|c| c.to_uppercase())
+//!     .flatten()
+//!     .collect::<String>();
+//! assert_eq!(&shouty_x, "LI ESTAS CXARMA");
+//!
+//! let first_converted_char = unicode_chars("mi portas cxapelon".chars())
+//!     .position(|c| !c.is_ascii());
+//! assert_eq!(first_converted_char, Some(10));
+//! ```
+
 // This is sorted by unicode value so we can do a binary chop
 static HATABLE_CHARS: [(char, char); 12] = [
     ('C', 'Ĉ'),
@@ -30,12 +71,22 @@ static UNHATABLE_CHARS: [(char, char); 12] = [
     ('ŭ', 'u'),
 ];
 
+/// An iterator adapter that returns the Unicode characters
+/// corresponding to the characters in the X-system from source
+/// iterator.
+///
+/// This `struct` is created by the [unicode_chars] function. See its
+/// documentation for more details.
 pub struct XToUnicode<I: Iterator<Item = char>> {
     queued_result: Option<Option<char>>,
     iter: I,
 }
 
 impl<I: Iterator<Item = char>> XToUnicode<I> {
+    /// Constructor to create the iterator.
+    ///
+    /// You can also use the [unicode_chars] function which is
+    /// probably more idiomatic.
     pub fn new(iter: I) -> Self {
         XToUnicode {
             queued_result: None,
@@ -88,12 +139,21 @@ impl<I: Iterator<Item = char>> Iterator for XToUnicode<I> {
     }
 }
 
+/// An iterator adapter that returns the X-system characters
+/// corresponding to the Unicode characters from the source iterator.
+///
+/// This `struct` is created by the [x_chars] function. See its
+/// documentation for more details.
 pub struct UnicodeToX<I: Iterator<Item = char>> {
     queued_char: Option<char>,
     iter: I,
 }
 
 impl<I: Iterator<Item = char>> UnicodeToX<I> {
+    /// Constructor to create the iterator.
+    ///
+    /// You can also use the [x_chars] function which is probably more
+    /// idiomatic.
     pub fn new(iter: I) -> Self {
         UnicodeToX {
             queued_char: None,
@@ -142,18 +202,92 @@ impl<I: Iterator<Item = char>> Iterator for UnicodeToX<I> {
     }
 }
 
+/// Adapts the character iterator `iter` to return characters with the
+/// X-system.
+///
+/// Returns an iterator that adapts the source iterator so that
+/// whenever an Esperanto special character is encountered its ASCII
+/// equivalent is returned followed by an X character with the same
+/// case as the special character.
+///
+/// See [unicode_chars] to go in the other direction.
+///
+/// # Examples
+///
+/// ```
+/// let mut iter = xsystem::x_chars("aĵo".chars());
+/// assert_eq!(iter.next(), Some('a'));
+/// assert_eq!(iter.next(), Some('j'));
+/// assert_eq!(iter.next(), Some('x'));
+/// assert_eq!(iter.next(), Some('o'));
+/// assert_eq!(iter.next(), None);
+/// ```
 pub fn x_chars<I: Iterator<Item = char>>(iter: I) -> UnicodeToX<I> {
     UnicodeToX::new(iter)
 }
 
+/// Adapts the character iterator `iter` to return Unicode characters
+/// instead of the X-system.
+///
+/// Returns an iterator that adapts the source iterator so that
+/// whenever an X is encountered following a letter that can have a
+/// hat on it in Esperanto the equivalent Unicode character is
+/// returned. The X’s in the source iterator can have either case
+/// regardless of the case of the previous letter. Any X’s that aren’t
+/// following an esperantisable letter are left alone.
+///
+/// See [x_chars] to go in the other direction.
+///
+/// # Examples
+///
+/// ```
+/// let mut iter = xsystem::unicode_chars("ajxo".chars());
+/// assert_eq!(iter.next(), Some('a'));
+/// assert_eq!(iter.next(), Some('ĵ'));
+/// assert_eq!(iter.next(), Some('o'));
+/// assert_eq!(iter.next(), None);
+/// ```
 pub fn unicode_chars<I: Iterator<Item = char>>(iter: I) -> XToUnicode<I> {
     XToUnicode::new(iter)
 }
 
+/// Converts a str slice coded with the X-system to a String with
+/// Unicode characters.
+///
+/// Returns a String where all of X’s in the source str slice that
+/// follow an esperantisable letter are converted to their Unicode
+/// equivalent. The X’s in the source string can have either case
+/// regardless of the case of the previous letter. Any X’s that aren’t
+/// following an esperantisable letter are left alone.
+///
+/// See [unicode_to_x] to go in the other direction.
+///
+/// # Examples
+///
+/// ```
+/// let hats = xsystem::x_to_unicode("ajxo");
+/// assert_eq!(hats, "aĵo".to_string());
+/// ```
 pub fn x_to_unicode(s: &str) -> String {
     unicode_chars(s.chars()).collect::<String>()
 }
 
+/// Converts a string slice by replacing all of the Esperanto letters
+/// with their equivalent in the X system.
+///
+/// Returns a String that is a copy of the string slice where any
+/// special Esperanto letters are replaced with their equivalent in
+/// the X-system. The case of the X will be the same as that of the
+/// special letter.
+///
+/// See [x_chars] to go in the other direction.
+///
+/// # Examples
+///
+/// ```
+/// let exes = xsystem::unicode_to_x("AĈA spicaĵo");
+/// assert_eq!(exes, "ACXA spicajxo".to_string());
+/// ```
 pub fn unicode_to_x(s: &str) -> String {
     x_chars(s.chars()).collect::<String>()
 }
